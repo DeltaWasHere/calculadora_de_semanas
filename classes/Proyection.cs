f2e4e019b8afa50a1c2b260869ab8f942f75e1b3
Taka <jegmontalvo@gmail.com>
2024-03-06 18:03:09 -0600

    solo calcular la pension si la persona tiene menos de 60 años, correccion de constante decimal

--- classes/Proyection.cs
+++ classes/Proyection.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -16,10 +17,10 @@ namespace calculadora_de_semanas
       
         private const int YEAR_DAYS = 365;
         private const int MONTHS = 12;
-        private int remainingWeeksTillSixties;
+        private BigDecimal remainingWeeksTillSixties;
         public Person person { get; }
         private BigDecimal BASIC_CUANTIC_PERCENT= 0.13;
-        private BigDecimal ANUAL_INCREMENT_FIRST_PERCENT = 0.245;
+        private BigDecimal ANUAL_INCREMENT_FIRST_PERCENT = 0.0245;
         private BigDecimal ANUAL_INCREMENT_SECOND_PERCENT = 0.11;
         public  BigDecimal FAMILIAL_ASSIGNATIONS_PERCENT = 0.15;
         public Proyection(Person person,int week, BigDecimal uma) {
@@ -33,54 +34,51 @@ namespace calculadora_de_semanas
         }
 
         public void calcRemainingWeeksTillSixties() {
-            if (person.age < 60)
-            {
-                BigDecimal aux = ((60 - person.age) * 365 / 7);
-                if (Regex.Match(aux.ToString(), @"\d+\.[5-9]").ToString().Length > 0)
-                {
-                    this.remainingWeeksTillSixties = int.Parse(aux.WholeValue.ToString()) + 1;
-                }
-                else
-                {
-                    this.remainingWeeksTillSixties = int.Parse(aux.WholeValue.ToString());
-                }
-            }
-            else
-            {
-                this.remainingWeeksTillSixties = 0;
+            BigDecimal aux;
+            if (this.person.age < 60) {
+                this.remainingWeeksTillSixties = ((60 - this.person.age) * 365 / 7)*(1);
+            } else{
+                remainingWeeksTillSixties = 0;
             }
         }
 
         public void calcTotalPension() {
-            bool substract = false;
-            BigDecimal anualSalary = BigDecimal.Parse(this.person.getSalarioPromedioDisplay()) * YEAR_DAYS;
-
-            BigDecimal basicCuanticPercent = (this.person.semanasTotales - 500);
-            basicCuanticPercent = ((basicCuanticPercent) / 52) / 100;
-            basicCuanticPercent = BigDecimal.Round(basicCuanticPercent, 9);
-
-            if (basicCuanticPercent.IsNegative()) { 
-                substract= true;
-                basicCuanticPercent = ( 500- this.person.getSemanasTotales()) / 52 / 100;
-            }
-            BigDecimal basicCuantia;
-            if (substract)
+            if (this.person.age < 60)
             {
-                basicCuantia=(anualSalary * BASIC_CUANTIC_PERCENT) -
-                    (anualSalary * BASIC_CUANTIC_PERCENT * basicCuanticPercent);
+                bool substract = false;
+                BigDecimal anualSalary = BigDecimal.Parse(this.person.getSalarioPromedioDisplay()) * YEAR_DAYS;
+
+                BigDecimal basicCuanticPercent = (this.person.semanasTotales - 500);
+                basicCuanticPercent = ((basicCuanticPercent) / 52) / 100;
+                basicCuanticPercent = BigDecimal.Round(basicCuanticPercent, 9);
+
+                if (basicCuanticPercent.IsNegative())
+                {
+                    substract = true;
+                    basicCuanticPercent = (500 - this.person.getSemanasTotales()) / 52 / 100;
+                }
+                BigDecimal basicCuantia;
+                if (substract)
+                {
+                    basicCuantia = (anualSalary * BASIC_CUANTIC_PERCENT) -
+                        (anualSalary * BASIC_CUANTIC_PERCENT * basicCuanticPercent);
+                }
+                else
+                {
+                    BigDecimal aux = (anualSalary * BASIC_CUANTIC_PERCENT * basicCuanticPercent);
+                    basicCuantia = (anualSalary * BASIC_CUANTIC_PERCENT) + aux;
+                }
+
+                BigDecimal anualIncrement = (anualSalary * ANUAL_INCREMENT_FIRST_PERCENT) *
+                    ((this.person.semanasTotales + remainingWeeksTillSixties - 500) / 52);
+                anualIncrement = anualIncrement + (anualIncrement * ANUAL_INCREMENT_SECOND_PERCENT);
+
+                this.pension = ((basicCuantia + anualIncrement) +
+                ((basicCuantia + anualIncrement) * FAMILIAL_ASSIGNATIONS_PERCENT)) / 12;
             }
             else {
-                BigDecimal aux = (anualSalary * BASIC_CUANTIC_PERCENT * basicCuanticPercent);
-                basicCuantia =(anualSalary * BASIC_CUANTIC_PERCENT) +aux;
-                   ;
+                this.pension = 0;
             }
-
-            BigDecimal anualIncrement = (anualSalary * ANUAL_INCREMENT_FIRST_PERCENT) * 
-                ((this.person.getSemanasTotales() + remainingWeeksTillSixties - 500) / 52);
-            anualIncrement = anualIncrement + (anualIncrement * ANUAL_INCREMENT_SECOND_PERCENT);
-
-             this.pension = ((basicCuantia+anualIncrement)+
-                ((basicCuantia + anualIncrement)*FAMILIAL_ASSIGNATIONS_PERCENT))/12;
         }
 
     }
